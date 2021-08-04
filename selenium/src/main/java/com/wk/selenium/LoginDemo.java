c65ce74c351844ceb2bc038f21882509e717940d
wangkai <1351182501@qq.com>
2021-08-04 18:12:32 +0800

    修改自动化测试工具

--- selenium/src/main/java/com/wk/selenium/LoginDemo.java
+++ selenium/src/main/java/com/wk/selenium/LoginDemo.java
@@ -1,12 +1,9 @@
 package com.wk.selenium;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import javax.annotation.PostConstruct;
 import java.util.concurrent.TimeUnit;
 
 public class LoginDemo {
@@ -18,7 +15,7 @@ public class LoginDemo {
     /**
      * 谷歌驱动
      **/
-    private static ChromeDriver browser;
+    private static ChromeDriver driver;
 
     /**
      * 账号
@@ -38,6 +35,7 @@ public class LoginDemo {
 //        loginMaYun();
 
         loginJunNet(url);
+        junNetQuote();
         Thread.sleep(500000);
         closeBrowser();
     }
@@ -49,14 +47,14 @@ public class LoginDemo {
         // 设置全局的驱动地址
         System.setProperty("webdriver.chrome.driver", "D:\\java\\driver\\chromedriver.exe");
         // 创建谷歌驱动对象
-        browser = new ChromeDriver();
+        driver = new ChromeDriver();
         // 浏览器实现自己模糊等待
-        browser.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        browser.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
 
     public static void closeBrowser() {
-        browser.quit();
+        driver.quit();
     }
 
     /**
@@ -65,9 +63,9 @@ public class LoginDemo {
      */
     public static String findLoginPage(String url) {
         // 打开网址
-        browser.get(url);
+        driver.get(url);
         // 找到登录注册进行点击跳转到真的登录页面
-        WebElement webElement = browser.findElementByLinkText("登录");
+        WebElement webElement = driver.findElementByLinkText("登录");
         if (webElement != null) {
             String realLoginUrl = webElement.getAttribute("href");
             // 模拟进行点击
@@ -85,16 +83,16 @@ public class LoginDemo {
         // 暂停3秒让页面先加载出来
         Thread.sleep(2000);
         // 输入账户和密码
-        browser.findElement(By.id("user_login")).sendKeys(USERNAME);
+        driver.findElement(By.id("user_login")).sendKeys(USERNAME);
         Thread.sleep(1000);
-        browser.findElement(By.id("user_password")).sendKeys(PASSWORD);
+        driver.findElement(By.id("user_password")).sendKeys(PASSWORD);
         Thread.sleep(1000);
         // 第一种一般情况下有的网站登录在输入完成之后可以按回车键进行直接登录
 //        browser.findElement(By.name("commit")).sendKeys(Keys.ENTER);
 
         // 第二种就是找到登录按钮进行模拟点击
         // 两个方法选择一个就可以了
-        WebElement loginWebElement = browser.findElement(By.name("commit"));
+        WebElement loginWebElement = driver.findElement(By.name("commit"));
         loginWebElement.click();
     }
 
@@ -105,35 +103,42 @@ public class LoginDemo {
     public static void loginJunNet(String url) throws InterruptedException {
         // 暂停2秒让页面先加载出来
         Thread.sleep(1000);
-        browser.get(url);
+        driver.get(url);
         // 找到登录注册进行点击跳转到真的登录页面
-        WebElement webElement = browser.findElementByCssSelector("[class='user-action-btn login']");
+        WebElement webElement = driver.findElementByCssSelector("[class='user-action-btn login']");
 //        WebElement webElement = browser.findElementByCssSelector(".user-action-btn.login");
         webElement.click();
         Thread.sleep(1000);
         // 点击供应商
 //        browser.switchTo().frame("layui-layer-iframe8");
-        browser.switchTo().frame(1);
+        driver.switchTo().frame(1);
         Thread.sleep(1000);
-        browser.findElementById("waiselected").click();
+        driver.findElementById("waiselected").click();
         Thread.sleep(1000);
         // 输入账户和密码
-        browser.findElement(By.id("loginid")).sendKeys("zjmiec");
+        driver.findElement(By.id("loginid")).sendKeys("zjmiec");
         Thread.sleep(500);
-        browser.findElement(By.id("loginpwd")).sendKeys("zjmiec1234");
+        driver.findElement(By.id("loginpwd")).sendKeys("zjmiec1234");
         Thread.sleep(500);
-        WebElement loginWebElement = browser.findElementByLinkText("登    录");
+        WebElement loginWebElement = driver.findElementByLinkText("登    录");
         loginWebElement.click();
         Thread.sleep(500);
         //转回主页面
-        browser.switchTo().defaultContent();
+        driver.switchTo().defaultContent();
     }
 
     /**
      * 军网报价
      */
-    public void junNetQuote() {
-        browser.switchTo().frame(0);
-        browser.findElementByCssSelector(".wait-handle-icon.quoted").click();
+    public static void junNetQuote() throws InterruptedException {
+        driver.switchTo().frame(0);
+        driver.findElementByCssSelector(".wait-handle-icon.quoted").click();
+        WebElement element = driver.findElementByCssSelector("[name=\"mhstate\"][value=\"1\"]");
+        System.out.println(element.getText());
+        driver.findElementById("productname_input").sendKeys("索尼（SONY） DSC-RX100M7 数码卡片机 黑色");
+        Thread.sleep(5);
+        driver.findElementByCssSelector("[class=\"ms-btn\"][href=\"javascript:void(0)\"]").click();
+        Thread.sleep(500);
+        driver.findElementByCssSelector("[productguid=\"03273ecf-db09-11eb-89a9-fefcfe9556b7\"]").click();
     }
 }
