444951871340634a5485f03b32a87e2b0e2ec095
mayadexintu <1351182501@qq.com>
2021-08-30 08:13:03 +0800

    修改rocketmq

--- mq/src/main/java/com/wk/mq/rocketmq/MyConsumer.java
+++ mq/src/main/java/com/wk/mq/rocketmq/MyConsumer.java
@@ -1,6 +1,7 @@
 package com.wk.mq.rocketmq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Service;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 //@RocketMQMessageListener(topic = "test-topic-1", consumerGroup = "my-consumer_test-topic-1")
-@RocketMQMessageListener(topic = "TEST_TOPIC", consumerGroup = "my-consumer_test-topic-1")
+@RocketMQMessageListener(topic = "RAFT_TOPIC", consumerGroup = "RAFT_CONSUMER_GROUP",messageModel= MessageModel.CLUSTERING)
 public class MyConsumer implements RocketMQListener<String> {
 
     @Override
--- mq/src/main/java/com/wk/mq/rocketmq/ProducerController.java
+++ mq/src/main/java/com/wk/mq/rocketmq/ProducerController.java
@@ -33,8 +33,8 @@ public class ProducerController {
         int num = 3;
         for (int i = 0; i < num; i++) {
             int nextInt = RandomUtils.nextInt();
-            SendResult result = rocketMQTemplate.syncSend("TEST_TOPIC", "my message-" + i + ":" + nextInt, 1000);
-            System.out.println("send result is\t" + result);
+            SendResult result = rocketMQTemplate.syncSend("RAFT_TOPIC", "my message-" + i + ":" + nextInt);
+            System.out.println("send result is：\t" + result);
         }
 
         map.put("isSend", "OK");
